632ded28c9d21ac14b7b4d4b9c8cf5a7efd8b9b5
Jemish <raininfotech01@gmail.com>
2021-02-20 09:41:25 +0530

    Initial Commit

---
+++ src/components/Demo.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3 v-for="i in users" :key=i>
+            {{i}}
+        </h3>
+        <button v-on:click="onsub()"> Event BTN</button>
+
+    </div>
+</template>
+
+
+<script>
+import func from '../../vue-temp/vue-editor-bridge';
+export default {
+    name: 'Demo',
+    props: ['users'],
+    methods:{
+        onsub(){
+            this.$emit("custEvent",["ABC","PQR","XYZ"])
+        }
+    },
+    created:function(){
+        console.log("Created")
+    },
+    beforeCreate:function(){
+        console.log("Before Create")
+    },
+    beforeMount:function(){
+        console.log("Before Mount")
+    }
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div v-bind:class={success:type,error:!type}>
+      Hello
+    </div>
+    <div v-bind:class=[arr]>
+      World
+    </div>
+    <div><button v-bind:class="[!type?'success':'error']">Submit</button></div>
+    <demo v-bind:users="users" v-on:custEvent="getData($event)" />
+  </div>
+</template>
+
+
+
+
+<script>
+
+import demo from '@/components/Demo.vue'
+
+export default {
+  components: {
+    demo
+  },
+  data(){
+    return{
+      type:0,
+      arr:'className',
+      users:["Jemish","Steve","Tony","Bucky","Natasha"]
+    }
+  },
+  methods : {
+    getData(data){
+        this.users = data;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.success{
+  color: blue;
+}
+.error{
+  color: red;
+}
+.className{
+  color:rgb(119, 123, 161);
+}
+</style>
